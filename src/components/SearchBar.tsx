11fbb8f24f3697054af55af1615dd62fb1f7b100
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-20 21:19:42 +0000

    created a radio group for different JSON data

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,8 +4,8 @@ const SearchBar = (props: SearchBarProps) => {
   const { filterParams, setFilterParams } = props
 
   return (<>
-    <div> Location Filter</div>
      <div className="searchBar">
+       <h3> Location Filter</h3>
         <input
           type="text"
           placeholder="Enter a location..."
@@ -15,7 +15,29 @@ const SearchBar = (props: SearchBarProps) => {
         />
       </div>
       <div className="talentPicker">
-        Select a field 
+        <h3>
+          Select JSON Data to filter
+        </h3>
+        <div>
+          <div>
+            Example
+            <input type="radio" id="html" name="example" value="Example"></input>
+          </div>
+          <div>
+            Models
+          <input type="radio" id="html" name="example" value="Example"></input>
+          </div>
+          <div>
+            Photographers
+            <input type="radio" id="html" name="example" value="Example"></input>
+          </div>
+          <div>
+            MUA
+            <input type="radio" id="html" name="example" value="Example"></input>
+          </div>
+        </div>
+        
+
       </div>
   
   </>)
